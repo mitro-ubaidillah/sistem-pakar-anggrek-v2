02d61a7784acd77a5bd6d1c482b811edfe53b8d7
mitro-ubaidillah <ubaidillahmitro@gmail.com>
2022-06-17 21:28:06 +0700

    dashboard

---
+++ resources/views/admin/index.blade.php
@@ -0,0 +1,35 @@
+@extends('layouts.main')
+@section('content')
+    <main class="mb-5">
+        <div class="container">
+            <div class="card  border-0 shadow rounded">
+                <div class="card-header">
+                    @include('partials.navAdmin')
+                </div>
+                <div class="card-body p-5">
+                    <div class="row">
+                        <div class="col-6 text-center">
+                            <img src="{{ asset('img/img-1.png') }}" alt="" width="500">
+                        </div>
+                        <div class="col-6 m-auto">
+                            <div class="row">
+                                <div class="col-6 mb-3">
+                                    <a href="{{ route('penyakit.index') }}" class="btn btn-custom-one"><i class="bi bi-journal-medical"></i> Lihat Penyakit yang terdaftar</a>
+                                </div>
+                                <div class="col-6 mb-3">
+                                    <a href="{{ route('gejala.index') }}" class="btn btn-custom-one"><i class="bi bi-clipboard-pulse"></i> Lihat Gejala yang terdaftar</a>
+                                </div>
+                                <div class="col-6 mb-3">
+                                    <a href="{{ route('cfUser.index') }}" class="btn btn-custom-one"><i class="bi bi-journal"></i> Lihat CF User yang terdaftar</a>
+                                </div>
+                                <div class="col-6 mb-3">
+                                    <a href="{{ route('penyakit.index') }}" class="btn btn-custom-one"><i class="bi bi-clipboard2-check"></i> Lihat Kasus yang terdaftar</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </main>
+@endsection
---
+++ resources/views/partials/navAdmin.blade.php
@@ -0,0 +1,15 @@
+<nav class="navbar navbar-expand-lg">
+    <div class="container-fluid">
+      <a class="navbar-brand font-color-default fw-semibold" href="/admin/dashboard">Admin Page</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+        <div class="navbar-nav ms-auto">
+          <a class="nav-link font-color-default fw-semibold" href="{{ route('penyakit.index') }}">Daftar Penyakit</a>
+          <a class="nav-link font-color-default fw-semibold" href="{{ route('gejala.index') }}">Daftar Gejala</a>
+          <a class="nav-link font-color-default fw-semibold" href="{{ route('cfUser.index') }}">Daftar CF User</a>
+        </div>
+      </div>
+    </div>
+</nav>
